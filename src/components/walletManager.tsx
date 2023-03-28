4e3f776b5146ab0658fbf6d1c8e63c5e02c27e1a
7i7o <mparij@gmail.com>
2023-03-27 21:56:40 -0300

    add getBalance

--- src/components/walletManager.tsx
+++ src/components/walletManager.tsx
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
-import { createWallet } from "permawebjs/wallet";
+import { createWallet, getBalance } from "permawebjs/wallet";
 import { type CreateWalletReturnProps } from "permawebjs/dist/types/wallet";
+
+const ENVIRONMENT = "local";
 
 const WalletManager: React.FC = () => {
   const [clicked, setClicked] = useState<boolean>(false);
   const [wallet, setWallet] = useState<CreateWalletReturnProps | null>(null);
+  const [clickedBalance, setClickedBalance] = useState<boolean>(false);
+  const [balance, setBalance] = useState<string | null>(null);
 
   const newWallet = async () => {
     try {
       console.log("Calling createWallet()");
       const wallet = await createWallet({
         seedPhrase: true,
-        environment: "local",
+        environment: ENVIRONMENT,
       });
       setWallet(wallet);
+    } catch {
+      console.log("Couldn't create new wallet");
+    }
+  };
+  const checkBalance = async () => {
+    try {
+      console.log("Calling getBalance()");
+      const balanceChecked = await getBalance({
+        address: wallet ? wallet.walletAddress : "",
+        environment: ENVIRONMENT,
+      });
+      setBalance(balanceChecked);
+      //   setClickedBalance(false);
     } catch {
       console.log("Couldn't create new wallet");
     }
@@ -34,8 +51,8 @@ const WalletManager: React.FC = () => {
             New Wallet
           </button>
         </div>
-        {clicked && (
-          <div className="col-span-1">
+        <div className="col-span-1">
+          {clicked && (
             <div className="card rounded-lg bg-base-200 p-4">
               {!wallet && <progress className="progress w-56"></progress>}
               {wallet && (
@@ -43,7 +60,9 @@ const WalletManager: React.FC = () => {
                   <p className="mb-2 w-64">
                     Your wallet address is:
                     <br />
-                    <strong className="break-all">{wallet.walletAddress}</strong>
+                    <strong className="break-all">
+                      {wallet.walletAddress}
+                    </strong>
                   </p>
                   {wallet.seedPhrase && (
                     <p className="mb-2 w-64">
@@ -55,8 +74,37 @@ const WalletManager: React.FC = () => {
                 </>
               )}
             </div>
-          </div>
-        )}
+          )}
+        </div>
+        <div className="col-span-1">
+          <button
+            className="btn-primary btn w-full"
+            disabled={
+              // clickedBalance ||
+              wallet ? false : true
+            }
+            onClick={() => {
+              setClickedBalance(true);
+              void checkBalance();
+            }}
+          >
+            Get Balance
+          </button>
+        </div>
+        <div className="col-span-1">
+          {clickedBalance && (
+            <div className="card rounded-lg bg-base-200 p-4">
+              {!balance && <progress className="progress w-56"></progress>}
+              {balance && (
+                <p className="mb-2 w-64">
+                  Your balance:&nbsp;
+                  {/* <br /> */}
+                  <strong className="break-all">{balance}</strong>
+                </p>
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
